6a8d5aba82d83078987618f37431e04fae3226eb
Asliddin Abdivasiyev <asliddin.abdivasiyev@gmail.com>
2026-09-03 14:05:28 +0500

    Setup nix and init project

---
+++ shell.nix
@@ -0,0 +1,43 @@
+{
+  pkgs ? let
+    lock = (builtins.fromJSON (builtins.readFile ./flake.lock)).nodes.nixpkgs.locked;
+    nixpkgs = fetchTarball {
+      url = "https://github.com/nixos/nixpkgs/archive/${lock.rev}.tar.gz";
+      sha256 = lock.narHash;
+    };
+  in
+    import nixpkgs {overlays = [];},
+  hpkgs ? pkgs.haskell.packages."ghc912",
+  pre-commit-check,
+  ...
+}:
+pkgs.stdenv.mkDerivation {
+  name = "flappy-bird-dev";
+
+  # Build time dependencies
+  nativeBuildInputs = [
+    hpkgs.ghc
+    hpkgs.ghcid
+    hpkgs.cabal-install
+    hpkgs.cabal-gild
+    hpkgs.hoogle
+    hpkgs.haskell-language-server
+    hpkgs.fourmolu
+    hpkgs.hlint
+    pkgs.just
+  ];
+
+  # Runtime dependencies
+  buildInputs = [
+    pre-commit-check.enabledPackages
+    pkgs.git
+    pkgs.pkg-config
+  ];
+  # Things to run before entering devShell
+  shellHook = ''
+    ${pre-commit-check.shellHook}
+  '';
+
+  # Environmental variables
+  NIX_CONFIG = "extra-experimental-features = nix-command flakes";
+}
